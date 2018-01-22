ab90516cc56a220868d31385108f7eb4a93ec5fd
Piotr Kazenas <piotr.kazenas@gmail.com>
2018-01-22 08:30:11 +0100

    Change library api a little bit

--- src/main/scala/pl/pkazenas/jsonschema4s/package.scala
+++ src/main/scala/pl/pkazenas/jsonschema4s/package.scala
@@ -12,15 +12,23 @@ package object jsonschema4s {
     case class ApiConfig(classLoader: ClassLoader = ClassLoader.getSystemClassLoader,
                          packagesToScan: List[String] = List())
 
-    def toModel(`type`: Type)(implicit apiConfig: ApiConfig = ApiConfig()): RootType = {
+    object Implicits {
+      implicit val defaultApiConfig: ApiConfig = ApiConfig()
+    }
+
+    def toModel(`type`: Type)(implicit apiConfig: ApiConfig): RootType = {
       implicit val classPathScanner: ClasspathScanner = ClasspathScanner(apiConfig.classLoader, apiConfig.packagesToScan)
       ModelExtractor.extract(`type`)
     }
 
-    def asModel[T](implicit tag: TypeTag[T], apiConfig: ApiConfig = ApiConfig() ): RootType = toModel(tag.tpe)(apiConfig)
+    def toModel[T](implicit tag: TypeTag[T], apiConfig: ApiConfig): RootType =
+      toModel(tag.tpe)(apiConfig)
 
-    def asJsonSchema[T](implicit tag: TypeTag[T], apiConfig: ApiConfig = ApiConfig()): String =
-      modelAsJsonSchema(asModel(tag))
+    def toJsonSchema(`type`: Type)(implicit apiConfig: ApiConfig): String =
+      modelAsJsonSchema(toModel(`type`))
+
+    def toJsonSchema[T](implicit tag: TypeTag[T], apiConfig: ApiConfig): String =
+      modelAsJsonSchema(toModel(tag.tpe))
 
     def modelAsJsonSchema(model: RootType): String = JsonSchemaPrinter(JsonSchemaGenerator.generate(model))
   }
